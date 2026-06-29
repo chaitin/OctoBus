451b0f35f59198159865c1a5ff76fe981354c93a
chaitin <chaitin@chaitin.com>
2026-06-29 13:38:06 +0800

    fix: register alertmanager-0-27-0 in octobus-tentacles

--- services/bin/octobus-tentacles.js
+++ services/bin/octobus-tentacles.js
@@ -425,6 +425,10 @@ const services = {
     entryFile: "../venus__tar/bin/venus-tar.js",
     serviceModule: "../venus__tar/src/service.js",
   },
+  "alertmanager-0-27-0": {
+    entryFile: "../prometheus__alertmanager_0-27-0/bin/alertmanager-0-27-0.js",
+    serviceModule: "../prometheus__alertmanager_0-27-0/src/service.js",
+  },
   "wangsu-label-ip": {
     entryFile: "../wangsu__label-ip/bin/wangsu-label-ip.js",
     serviceModule: "../wangsu__label-ip/src/service.js",
